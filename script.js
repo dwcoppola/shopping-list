1b5c16b89c304ec558eb3d4add84254df3ee562f
David <66121421+dwcoppola@users.noreply.github.com>
2021-03-25 11:51:53 -0400

    added local storage disclaimer and buildTable()

--- script.js
+++ script.js
@@ -1,81 +1,51 @@
-function pullListFromStorage() {
-    var stockArray = localStorage['list'].split(",");
+function pullListFromStorage() 
+{
+    var stockArray = localStorage['shopping-list-list-dwcoppola'].split(",");
     return stockArray.slice(0, stockArray.length - 1);
 }
 
-function localStorageCheck() {
-    if (localStorage['history'] === undefined) {
-        localStorage['history'] = '';
+function localStorageCheck() 
+{
+    if (localStorage['shopping-list-history-dwcoppola'] === undefined) {
+        localStorage['shopping-list-history-dwcoppola'] = '';
     }
-    if (localStorage['checked'] === undefined) {
-        localStorage['checked'] = '';
+    if (localStorage['shopping-list-checked-dwcoppola'] === undefined) {
+        localStorage['shopping-list-checked-dwcoppola'] = '';
     } 
-    if (localStorage['list'] === undefined) {
-        localStorage['list'] = '';
-    } else {
+    if (localStorage['shopping-list-list-dwcoppola'] === undefined) {
+        localStorage['shopping-list-list-dwcoppola'] = '';
+    } 
+    if (localStorage['shopping-list-user-decision-dwcoppola'] === undefined) {
+        localStorage['shopping-list-user-decision-dwcoppola'] = '';
+    }
+    else {
         return pullListFromStorage();
     }
 }
 
-function addToList() {
-    var item = document.getElementById('new-item-input').value;
-    var item = item.replaceAll(",", " ").trim();
-    if (localStorage['list'].split(',').includes(item.toLowerCase())) {
-        alert("That's already in your list");
-        location.reload();
-    } else {
-        localStorage['list'] += item.toLowerCase() + ",";
-        localStorage['checked'] += (item.toLowerCase() + "," + '0,');
-        caption = `Added ${item.toLowerCase()}`;
-        localStorage['history'] = caption.toLowerCase();
-        location.reload();  
-    }
-}
-
-function removeFromStock(item) {
-    deleteItem = item + ',';
-    localStorage['list'] = localStorage['list'].replace(deleteItem, '');
-    if (localStorage['checked'].includes(deleteItem + '0,')) {
-        localStorage['checked'] = localStorage['checked'].replace(deleteItem + '0,', '');
-    } else {
-        localStorage['checked'] = localStorage['checked'].replace(deleteItem + '1,', '');
-    }
-    caption = `Removed ${initialCaps(item)}`;
-    localStorage['history'] = caption.toLowerCase();
-    location.reload();
-}
-
-function addElement(
-    parent, child, content, 
-    attribute1=null, attibuteValue1=null, 
-    attribute2=null, attributeValue2=null, 
-    attribute3=null, attributeValue3=null,
-    ) {
-    var parent = document.querySelector(parent);
-    var child = document.createElement(child);
-    parent.appendChild(child);
-    child.textContent = content;
-    if (attribute1 !== null && attibuteValue1 !== null) {
-        child.setAttribute(attribute1, attibuteValue1);
-    };
-    if (attribute2 !== null && attributeValue2 !== null) {
-        child.setAttribute(attribute2, attributeValue2);
-    };
-    if (attribute3 !== null && attributeValue3 !== null) {
-        child.setAttribute(attribute3, attributeValue3);
-    };
-}
-
-function initialCaps(str) {
+function initialCaps(str) 
+{
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function makeStockList(list) {
-    addElement('#table-container', 'table', '', 'id', 'stock-list');
-    addElement('table', 'tr', '', 'id', 'top-row');
-    addElement('#top-row', 'th');
-    addElement('#top-row', 'th');
-    addElement('#top-row', 'th');
+function adjustHistoryCaption(text) 
+{
+    localStorage['shopping-list-history-dwcoppola'] = text.toLowerCase();
+    var caption = document.querySelector('#history-caption');
+    caption.innerHTML = text;
+}
+
+function removeAllChildNodes(parent) 
+{
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function buildTable(list) 
+{
+    table = document.querySelector('table');
+    removeAllChildNodes(table);
     for (item in list) {
         addElement('table', 'tr', '', 'id', `row-${item}`);
         addElement(
@@ -95,7 +65,8 @@ function makeStockList(list) {
             'id', `checkbox-${item}`
         );
         var checkbox = document.getElementById(`checkbox-${item}`);
-        localStorage['checked'].includes(`${list[item].toLowerCase()},1`) ? checkbox.checked = true : checkbox.checked = false;
+        localStorage['shopping-list-checked-dwcoppola'].includes(`${list[item].toLowerCase()},1`) ? 
+            checkbox.checked = true : checkbox.checked = false;
         addElement(
             `#row-${item}`, 'td', '', 
             'class', 'delete-button', 
@@ -109,17 +80,82 @@ function makeStockList(list) {
     }
 }
 
-function listenForCheck(checkBoxID) {
-    checkbox = document.getElementById(`checkbox-${checkBoxID}`);
-    checkedItem = document.getElementById(`list-item-${checkBoxID}`).textContent.toLowerCase();
-    if (checkbox.checked === true) {
-        localStorage['checked'] = localStorage['checked'].replace(checkedItem + ',0', checkedItem + ',1');
-        localStorage['history'] = `checked ${checkedItem}`;
-        location.reload();
+function addToList() 
+{
+    var item = document.getElementById('new-item-input').value;
+    var item = item.replaceAll(",", " ").trim();
+    if (localStorage['shopping-list-list-dwcoppola'].split(',').includes(item.toLowerCase())) {
+        alert("That's already in your list");
     } else {
-        localStorage['checked'] = localStorage['checked'].replace(checkedItem + ',1', checkedItem + ',0');
-        localStorage['history'] = `unchecked ${checkedItem}`;
-        location.reload();
+        localStorage['shopping-list-list-dwcoppola'] += item.toLowerCase() + ",";
+        localStorage['shopping-list-checked-dwcoppola'] += (item.toLowerCase() + "," + '0,');
+        adjustHistoryCaption(`Added ${initialCaps(item)}`);
+        buildTable(pullListFromStorage());
+        var clearItem = document.getElementById('new-item-input')
+        clearItem.value = '';
+    }
+}
+
+function removeFromStock(item) 
+{
+    var deleteItem = item + ',';
+    localStorage['shopping-list-list-dwcoppola'] = localStorage['shopping-list-list-dwcoppola'].replace(deleteItem, '');
+    if (localStorage['shopping-list-checked-dwcoppola'].includes(deleteItem + '0,')) {
+        localStorage['shopping-list-checked-dwcoppola'] = localStorage['shopping-list-checked-dwcoppola'].replace(deleteItem + '0,', '');
+    } else {
+        localStorage['shopping-list-checked-dwcoppola'] = localStorage['shopping-list-checked-dwcoppola'].replace(deleteItem + '1,', '');
+    }
+    adjustHistoryCaption(`Removed ${initialCaps(item)}`);
+    buildTable(pullListFromStorage());
+}
+
+function addElement(parent, child, content, 
+                        attribute1=null, attibuteValue1=null, 
+                            attribute2=null, attributeValue2=null, 
+                                attribute3=null, attributeValue3=null) 
+{
+        var parent = document.querySelector(parent);
+        var child = document.createElement(child);
+        parent.appendChild(child);
+        child.textContent = content;
+        if (attribute1 !== null && attibuteValue1 !== null) {
+            child.setAttribute(attribute1, attibuteValue1);
+        };
+        if (attribute2 !== null && attributeValue2 !== null) {
+            child.setAttribute(attribute2, attributeValue2);
+        };
+        if (attribute3 !== null && attributeValue3 !== null) {
+            child.setAttribute(attribute3, attributeValue3);
+        };
+}
+
+function makeStockList(list) {
+    addElement('#table-container', 'table', '', 'id', 'stock-list');
+    addElement('table', 'tr', '', 'id', 'top-row');
+    addElement('#top-row', 'th');
+    addElement('#top-row', 'th');
+    addElement('#top-row', 'th');
+    buildTable(list);
+}
+
+function focusOnElement(elementID) {
+    var e = document.getElementById(elementID);
+    e.focus();
+}
+
+function listenForCheck(checkBoxID) {
+    var checkbox = document.getElementById(`checkbox-${checkBoxID}`);
+    var checkedItem = document.getElementById(`list-item-${checkBoxID}`).textContent.toLowerCase();
+    if (checkbox.checked === true) {
+        localStorage['shopping-list-checked-dwcoppola'] = localStorage['shopping-list-checked-dwcoppola'].replace(checkedItem + ',0', checkedItem + ',1');
+        localStorage['shopping-list-history-dwcoppola'] = `checked ${checkedItem}`;
+        var caption = document.querySelector('#history-caption');
+        caption.innerHTML = initialCaps(localStorage['shopping-list-history-dwcoppola']);
+    } else {
+        localStorage['shopping-list-checked-dwcoppola'] = localStorage['shopping-list-checked-dwcoppola'].replace(checkedItem + ',1', checkedItem + ',0');
+        localStorage['shopping-list-history-dwcoppola'] = `unchecked ${checkedItem}`;
+        var caption = document.querySelector('#history-caption');
+        caption.innerHTML = initialCaps(localStorage['shopping-list-history-dwcoppola']);
     }
 }
 
@@ -131,23 +167,51 @@ function listenForEnter(event) {
     };
 }
 
-function focusOnElement(elementID) {
-    var e = document.getElementById(elementID);
-    e.focus();
+function userDecisionCheck() {
+    if (localStorage['shopping-list-user-decision-dwcoppola'] === '0') {
+        localStorage.removeItem('shopping-list-history-dwcoppola');
+        localStorage.removeItem('shopping-list-checked-dwcoppola');
+        localStorage.removeItem('shopping-list-list-dwcoppola');
+        localStorage.removeItem('shopping-list-user-decision-dwcoppola');
+    }
 }
 
-function buildOrRebuildPage() {
-    localStorageCheck();
-    addElement(
-        'body', 'h2', 'Shopping List');
+function welcome() {
+    if (localStorage['shopping-list-user-decision-dwcoppola'] === '') {
+        confirmation = confirm(
+`Selecting 'OK' will allow the use of local storage.
+If you don't want to save your progress for later use, 
+please select 'CANCEL' or close this window and any 
+storage used during your session will be deleted once 
+you're finished.`);
+        if (confirmation === false) {
+            localStorage['shopping-list-user-decision-dwcoppola'] = '0';
+        } else {
+            localStorage['shopping-list-user-decision-dwcoppola'] = '1';
+        }
+    }
+}
 
+// The app
+
+function buildPage() 
+{
+    localStorageCheck();
+    welcome();
+    {
+        var body = document.querySelector('body');
+        body.setAttribute('onbeforeunload', 'userDecisionCheck()')
+    }
+    addElement(
+        'body', 'h2', 'Shopping List'
+    );
     addElement(
         'body', 'div', '', 'id', 'input-controls');
     addElement('#input-controls', 'input', '', 
         'placeholder', 'Name of New Item', 
         'id', 'new-item-input', 
         'onkeydown', 'listenForEnter(event)'
-        );
+    );
     {
         var input = document.getElementById('new-item-input');
         input.setAttribute('maxlength', '15');
@@ -156,17 +220,16 @@ function buildOrRebuildPage() {
     addElement('#input-controls', 'button', 'Add', 
         'onclick', 'addToList()', 
         'id', 'add-item-button'
-        );
-        addElement('body', 'div', '', 'id', 'caption-container');
-        addElement('#caption-container', 'p', initialCaps(localStorage['history']), 'id', 'history-caption');
-    /*
-    addElement('#input-controls', 'button', 'Sort', 
-        'onclick', 'sortList()', 
-        'id', 'sort-list-button'
-        );
-    */
+    );
+    addElement('body', 'div', '', 'id', 'caption-container');
+    addElement(
+        '#caption-container', 'p', initialCaps(localStorage['shopping-list-history-dwcoppola']), 
+        'id', 'history-caption'
+    );
     addElement('body', 'div', '', 'id', 'table-container');
     makeStockList(pullListFromStorage());
 }
 
-buildOrRebuildPage();
+// Run the app
+
+buildPage();
